b5cfc2ec47a1e20ac83fd5ec43f695f09fe5d336
Maniph1997 <maniph8728@gmail.com>
2024-03-30 13:02:46 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Classes from './components/ClassComponent/Classes';
+import Tech from './components/TeacherComponent/Tech';
+import Student from './components/StudentComponent/Stutent';
+import Home from './components/Home/Home';
+import { Routes, Route } from "react-router-dom"
+
+
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Tech/> */}
+      {/* <Classes />
+      <Student /> */}
+      {/* <Home /> */}
+      <Home />
+
+      <Routes>
+        <Route path='/class' element={<Classes />} />
+        <Route path='/student' element={<Student />} />
+        <Route path='/teacher' element={<Tech />} />
+
+      </Routes>
+
+
+
+
+
     </div>
   );
 }
 
-export default App;
+export default App; 
